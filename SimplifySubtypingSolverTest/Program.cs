2c541ea8be6eebccfd3cf95f0b359dfe13b35de4
natami <milovanat24@gmail.com>
2020-05-07 15:23:04 +0300

    Initial commit

---
+++ SimplifySubtypingSolverTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimplifySubtypingSolverTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
